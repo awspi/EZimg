f1fcbbca190904beefc24a8638199d9c5e758e50
awspi <206002328@qq.com>
2022-08-05 00:01:30 +0800

    feat: init project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # pithy-front
- vue3 前中台综合项目
+
+vue3 前中台综合项目
+by pithy
+
+# Vue 3 + Vite
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  //tailwind 应用
+  content: ['./index.html', './src/**/*.{vue,js}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
