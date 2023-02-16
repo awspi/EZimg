dcd985aa0380eec596ec9aadc709d198cb1359ae
awspi <206002328@qq.com>
2023-02-16 18:50:56 +0800

    fix: vercel跨域

--- vite.config.js
+++ vite.config.js
@@ -19,15 +19,15 @@ export default defineConfig({
     }
   },
   //代理配置
-  server: {
-    proxy: {
-      // 代理所有 /api 的请求，该求情将被代理到 target 中
-      '/api': {
-        // 代理请求之后的请求地址
-        target: 'https://api.imooc-front.lgdsunday.club/',
-        // 跨域
-        changeOrigin: true
-      }
-    }
-  }
+  // server: {
+  //   proxy: {
+  //     // 代理所有 /api 的请求，该求情将被代理到 target 中
+  //     '/api': {
+  //       // 代理请求之后的请求地址
+  //       target: 'https://api.imooc-front.lgdsunday.club/',
+  //       // 跨域
+  //       changeOrigin: true
+  //     }
+  //   }
+  // }
 })
